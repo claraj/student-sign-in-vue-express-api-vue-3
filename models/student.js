2698c3199ce185bca9b0d60533020ff9304f0535
clara <10088152+claraj@users.noreply.github.com>
2021-03-27 17:40:24 -0500

    star id regex validation

--- models/student.js
+++ models/student.js
@@ -9,7 +9,12 @@ module.exports = (sequelize, DataTypes) => {
         starID: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                is: /^[a-z]{2}\d{4}[a-z]{2}$/  
+                // Regular expression to match the pattern of a StarID, two lowercase letters a-z, four digits, two lowercase letters a-z
+                // https://regex101.com/r/GDMDuY/1/
+            }
         }, 
         
         present: {
